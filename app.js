524e6698985e729640c0fb6740cd145f726f065d
katherinenholland <katherinenholland@gmail.com>
2014-07-16 15:39:06 -0400

    app.js, index.html, main.css
    Updates to all files to change to a responsive design

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 $(document).ready(function() {
-    $(".button1").click(function() {
-        $(".photo1").show();
-        $(".photo2").hide();
-        $(".button1").css("background-color","#333");
-        $(".button2").css("background-color", "#555");
+    // populate the select
+    $.each(ImageList, function (key) {
+        //alert(key);
+        $(".selectId").append("<option>" + key + "</option>");
     });
 
-    $(".button2").click(function() {
-        $(".photo2").show();
-        $(".photo1").hide();
-        $(".button2").css("background-color","#333");
-        $(".button1").css("background-color", "#555");
+    // populate the li buttons
+    $.each(ImageList[$(".selectId").val()], function(keyItem) {
+        $(".indexList").append("<li value='" + keyItem + "'>" + ImageList[$(".selectId").val()][keyItem].Title + "</li>"); 
     });
 
+    // li button click listener
+    $(".indexList").on('click', 'li', function() {
+        //alert("you clicked on " + $(this).text());
+        $(".selectedPhoto").html("<img src=" + ImageList[$(".selectId").val()][$(this).val()].Image + ">");
+        $(".title").html("<h3>" + ImageList[$(".selectId").val()][$(this).val()].Title + "</h3>");
+    });
+
+    
+    // select element onChange
     $(".selectId").change(function() {
-        var selectedValue = $(this).find(":selected").val();
-        if (selectedValue == "1") {
-            $(".photo1").html("<img src=" + ImageList.Lions[0].Image + ">");
-            $(".photo2").html("<img src=" + ImageList.Lions[1].Image + ">");
-            $(".option1").html(ImageList.Lions[0].Title);
-            $(".option2").html(ImageList.Lions[1].Title);
-        }
-
-        if (selectedValue == "2") {
-            $(".photo1").html("<img src=" + ImageList.Tiger[0].Image + ">");
-            $(".photo2").html("<img src=" + ImageList.Tiger[1].Image + ">");
-            $(".option1").html(ImageList.Tiger[0].Title);
-            $(".option2").html(ImageList.Tiger[1].Title);
-        }
-
-        if (selectedValue == "3") {
-            $(".photo1").html("<img src=" + ImageList.Bears[0].Image + ">");
-            $(".photo2").html("<img src=" + ImageList.Bears[1].Image + ">");
-            $(".option1").html(ImageList.Bears[0].Title);
-            $(".option2").html(ImageList.Bears[1].Title);
-        }
+        //alert($(".selectId").val());
+        $(".indexList").empty();
+        $.each(ImageList[$(".selectId").val()], function(keyItem) {
+            //alert(ImageList[$(".selectId").val()][keyItem].Title);
+            $(".indexList").append("<li value='" + keyItem + "'>" + ImageList[$(".selectId").val()][keyItem].Title + "</li>");
+        });
     });
 }); //end of document ready
